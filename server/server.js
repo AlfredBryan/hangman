b26325067e4d8bc9377d7fe2a1d89d4db9a23034
AlfredBryan <alfred.chimereze@gmail.com>
2019-09-27 09:00:57 +0100

    heroku build scripts

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const dotenv = require("dotenv");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const logger = require("morgan");
+const path = require("path");
 const mongoose = require("mongoose");
 const router = require("./routes/router");
 
@@ -19,6 +20,16 @@ mongoose
   })
   .then(() => console.log("Connected to DB"))
   .catch(error => console.log("Error" + error));
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
